1aceb57e5a3d6144e4b0a15bac823932b6e89bda
abstraction <16391550+abstraction@users.noreply.github.com>
2022-01-27 13:12:21 +0530

    partially fixed timeline conflicts

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Fitbit Sleep Transpiler
+# Fitbit Sleep Wrangler
 
-Convert Fitbit sleep data to CSV.
+Transform Fitbit sleep data to CSV. With millenium day, start time and end time as columns.
 
 e.g.
 | Millenium day | Start time | End time |
@@ -8,6 +8,10 @@ e.g.
 | 8024 | 20:40 | 21:00 |
 | 8024 | 21:20 | 21:35 |
 
-## TODOs
+## Features
 
-- [ ] Fix
+- Create CSV with columns:
+  - Millenium day (days from 2000-01-01)
+  - Sleep time
+  - Wake time
+- Fix timeline conflicts (beta)
--- src/index.js
+++ src/index.js
@@ -1,80 +1,98 @@
-const fs = require('fs');
-const path = require('path');
-const axios = require('axios').default;
 const addSeconds = require('date-fns/addSeconds');
 const formatDistanceStrict = require('date-fns/formatDistanceStrict');
 const format = require('date-fns/format');
+const axios = require('axios').default;
+const fs = require('fs');
+const path = require('path');
 
-const dataCSV = [];
-const dataPath = path.join(__dirname, '../src/');
-const log = (...args) => console.log(args);
-const milleniumDay = '1999-12-31'; // 2000-01-01
+const startDate = '2022-01-07';
+const endDate = '2022-01-27';
+const accessToken = require('./config').accessToken;
 
-const dataBuffer = fs.readFileSync(dataPath + 'data.json');
-const dataJSON = JSON.parse(dataBuffer);
+const resolve = (p) => path.join(__dirname, p);
 
-for (sleep of dataJSON.sleep) {
-  const dayLog = [];
-  for (sleepLog of sleep.levels.data) {
-    const timestamp = sleepLog.dateTime;
-    const sleepState = sleepLog.level;
-    const sleepDuration = sleepLog.seconds;
-
-    if (sleepState !== 'restless' && sleepState !== 'awake' && sleepState !== 'wake') {
-      const milleniumDate = parseInt(
-        formatDistanceStrict(new Date(milleniumDay), new Date(timestamp), {
-          unit: 'day',
-        })
-      );
-      const sleepStartHhMm = format(new Date(timestamp), 'HH:mm');
-      const sleepEnd = addSeconds(new Date(timestamp), sleepDuration);
-      const sleepEndHhMm = format(new Date(sleepEnd), 'HH:mm');
-      const row = milleniumDate + ',' + sleepStartHhMm + ',' + sleepEndHhMm;
-      dayLog.push(row);
-    }
-  }
-  dataCSV.unshift(dayLog);
-}
-
-// const dataCSVb = dataCSV;
-
-// const fixTimeline = () => {
-//   newTimeline = [];
-//   for(day of dataCSVb) {
-//     // day.forEach((block, idx, day) => {
-//     for(let log in day){
-//       const segment = day[log].split(',');
-//       const prevSegment = (day[log - 1] !== undefined) ? day[log - 1].split(',') : null;
-//       if (prevSegment !== null && segment[1] === prevSegment[2] && segment[0] === prevSegment[0]) {
-//         day.shift();
-//         newTimeline.push(segment[0] + ',' + prevSegment[1] + ',' + segment[2]);
-//       } else {
-//         newTimeline.push(day[log]);
-//         // day.shift();
-//       }
-//     }
-//     // })
-//   }
-//   return dataCSVb;
-// }
-
-
-
-// const sleepCSV = fixTimeline();
-
-const tempCSVf = () => {
-  newTimeline = [];
-  for(day of dataCSV) {
-    day.forEach((block) => {
-      newTimeline.push(block);
+const getMilleniumDiff = (date) => {
+  const milDay = new Date('1999-12-31'); // 2000-01-01 (To include end date)
+  return parseInt(
+    formatDistanceStrict(date, milDay, {
+      roundingMethod: 'floor', // by defaut 23:45 of a day will be converted to the next day
+      unit: 'day',
     })
+  );
+};
+
+// TODO - Remove date-fns
+const wrangleData = (data) => {
+  const sleep = [];
+  const awake = ['restless', 'awake', 'wake'];
+  for (let sleepData of data) {
+    const day = [];
+    const sleepTimeline = sleepData.levels.data;
+    for (let sleepBlock of sleepTimeline) {
+      if (!awake.includes(sleepBlock.level)) {
+        const startBlock = new Date(sleepBlock.dateTime + 'Z'); // "Z" to make timezone offset complete otherwise js will infer timezone
+        const endBlock = new Date(addSeconds(startBlock, sleepBlock.seconds));
+        const daysFromMillenium = getMilleniumDiff(startBlock);
+        let startTime = startBlock.toJSON().slice(11, 16); // date-fns format parses in my timezone, experiment later
+        let endTime = endBlock.toJSON().slice(11, 16);
+        const block = [daysFromMillenium, startTime, endTime];
+        day.push(block);
+      }
+    }
+    sleep.unshift(day);
   }
-  return newTimeline;
-}
+  return sleep;
+};
 
-const tempCSV = tempCSVf();
+// TODO - edge case of midnight conflict is yet to be fix
+/**
+ * [ 8045, '22:12', '00:01' ],
+ * [ 8046, '00:01', '00:53' ],
+ */
 
-// console.log(dataCSV);
-// fs.writeFileSync(path.join(__dirname, '../src/sleepCSV.csv'), sleepCSV.join('\n'));
-fs.writeFileSync(path.join(__dirname, '../src/tempCSV.csv'), tempCSV.join('\n'));
+const fixTimeline = (sleep) => {
+  let fixed = [];
+  let flag = true;
+  sleep = sleep.flat();
+  let i = 0;
+  while (flag) {
+    flag = false;
+    while (sleep.length > 1 && sleep[1] !== undefined) {
+      if (sleep[0][1] === sleep[0][2]) sleep.splice(0, 1);
+      else if (sleep[0][0] === sleep[1][0] && sleep[0][2] === sleep[1][1]) {
+        fixed.push([sleep[0][0], sleep[0][1], sleep[1][2]]);
+        if (i > 0) fixed.splice(0, 2);
+        sleep.splice(0, 2);
+        flag = true;
+      } else {
+        fixed.push(sleep[0]);
+        if (i > 0) fixed.splice(0, 1);
+        sleep.splice(0, 1);
+      }
+    }
+    sleep = [...fixed];
+    i++;
+  }
+  return fixed;
+};
 
+axios
+  .get(
+    `https://api.fitbit.com/1.2/user/-/sleep/date/${startDate}/${endDate}.json`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  )
+  .then((res) => {
+    const sleep = wrangleData(res.data.sleep);
+
+    fs.writeFileSync(
+      path.join(__dirname, '../data/fixed.csv'),
+      fixTimeline(sleep).join('\n')
+    );
+  })
+  .catch((e) => {
+    console.log('Error getting sleep data');
+  });
